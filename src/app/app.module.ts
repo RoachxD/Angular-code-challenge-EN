7d737148b49b22ca833b63625df405b55d6cb6ad
RoachxD <nicolae.adrian.m@gmail.com>
2023-06-13 10:49:27 +0300

    Exchanged `FormsModule` for `ReactiveFormsModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, ReactiveFormsModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
